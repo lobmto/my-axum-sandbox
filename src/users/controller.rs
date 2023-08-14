b911050b5ca67afaee5cfae190f60781e3e69c33
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-08-15 02:50:25 +0900

    refactor: move `create_users` to controller

---
+++ src/users/controller.rs
@@ -0,0 +1,30 @@
+use axum::{http::StatusCode, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+
+pub fn user_routes() -> Router {
+    Router::new().route("/", post(create_user))
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+pub struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+pub struct User {
+    id: u64,
+    username: String,
+}
