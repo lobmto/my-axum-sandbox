46e3b6bc91646db89b6664bd90f1fbe4f39861c8
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-08-15 11:21:27 +0900

    refactor: add `dto.rs`

--- src/users/controller.rs
+++ src/users/controller.rs
@@ -1,5 +1,5 @@
+use super::dto::{CreateUser, User};
 use axum::{http::StatusCode, Json};
-use serde::{Deserialize, Serialize};
 
 pub async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
     // insert your application logic here
@@ -11,16 +11,4 @@ pub async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<U
     // this will be converted into a JSON response
     // with a status code of `201 Created`
     (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Deserialize)]
-pub struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-pub struct User {
-    id: u64,
-    username: String,
 }
--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,6 +1,7 @@
 use axum::{routing::post, Router};
 
 pub mod controller;
+pub mod dto;
 
 pub fn routes() -> Router {
     Router::new().route("/", post(controller::create_user))
