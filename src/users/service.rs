219fd00b425e626f5574703b708dfe75fd2c162f
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-08-15 16:49:11 +0900

    refactor: re-export `service::error`

--- src/users/service.rs
+++ src/users/service.rs
@@ -5,14 +5,14 @@ use crate::users;
 
 #[async_trait]
 pub trait Service {
-    async fn create_user(&self, payload: CreateUser) -> Result<users::Entity, Error>;
+    async fn create_user(&self, payload: CreateUser) -> Result<users::Entity, service::Error>;
 }
 
 pub struct ServiceImpl {}
 
 #[async_trait]
 impl Service for ServiceImpl {
-    async fn create_user(&self, payload: CreateUser) -> Result<users::Entity, Error> {
+    async fn create_user(&self, payload: CreateUser) -> Result<users::Entity, service::Error> {
         Ok(users::Entity {
             id: 1234,
             username: payload.username,
@@ -20,8 +20,11 @@ impl Service for ServiceImpl {
     }
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("Unknown error: {0}")]
-    UnknownError(String),
+pub use service::Error;
+mod service {
+    #[derive(Debug, thiserror::Error)]
+    pub enum Error {
+        #[error("Unknown error: {0}")]
+        UnknownError(String),
+    }
 }
